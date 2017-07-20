3f4a12f03484a0ef405d797d492d552ec1051fc2
DalvirSinghBains <dalvirsinghbains326@gmail.com>
2017-07-20 19:02:33 -0400

    Connected all the modules for (alpha calculation,pi calculation and final length calculation).All the modules are correctly working.

--- .idea/LengthCalculator.py
+++ .idea/LengthCalculator.py
@@ -5,17 +5,26 @@
 # The  output value produced is length by the above formula
 #degrees=radians*(180/pi)
 import math
+import AlphaCalculator
 
-def lengthCalculator(r):
- print('Enter the value of radius:')
- r=input()
- alpha=2.31
- degrees=alpha*(180/math.pi)
- #print(degrees)
- #print(math.cos(degrees))
- g=math.cos(alpha/2)
- print(g)
- l= 2*r*(1-math.cos(alpha/2))
- print('The value of length is:')
- print(l)
-lengthCalculator(2)
+class LegthCalculator(object):
+
+  def calculateLength(self):
+     #The user is prompted to enter the value of radius at runtime
+     print('Enter the value of radius:')
+     radius=input()
+
+     #the value of alpha used in the equation is in radians
+     #the cosine function included in the Python math library is used for calculating cos(x)
+     length= (2.0*radius*(1.0-math.cos(AlphaCalculator.alphaEstimateRadians/2.0)))
+     length1 = (2.0 * radius * (1.0 - math.cos(AlphaCalculator.alphaEstimateDegrees/ 2.0)))
+
+     print'The value of length is:'+str(length)+'(alpha in radians)'
+     print'The value of length is:'+ str(length1)+'(alpha in degrees)'
+
+def main():
+ lengthCalculatorObject=LegthCalculator()
+ lengthCalculatorObject.calculateLength()
+
+
+main()
--- .idea/PiCalculator.py
+++ .idea/PiCalculator.py
@@ -1,20 +1,26 @@
 #use the Gregory-Leibniz series for the calculation of Pi
 #pi=4(1-1/3+1/5-1/7+1/9-1/11+...........)
+#here the loops are iterated 10^6 times
+#to get the value of pi correct to 7 decimal places # pi=3.14159215359
 
-def pi_calculator(terms):
- positive_sum = 0.0
- negative_sum = 0.0
+class PiApproximation(object):
 
- for n in range(1,terms,1):
-    positive_sum += 1/((4*n)-3.0)
- print(positive_sum) #return positive_sum #4.30098911394
+ def piCalculator(self):
+   positiveSum = 0.0
+   negativeSum = 0.0
+   terms = 1000000
 
- for n in range(1,terms,1):
-    negative_sum += 1/((4*n)-1.0)
- print(negative_sum) #return negative_sum #3.14159260358
+   for n in range(1,terms,1):
+    positiveSum += 1/((4*n)-3.0)
+   for n in range(1,terms,1):
+     negativeSum += 1/((4*n)-1.0)
 
- pi_estimate=4*(positive_sum-negative_sum)
- print(pi_estimate)
+   piEstimate=4*(positiveSum-negativeSum)
+   return piEstimate
 
-pi_calculator(10000000) #here the loops are iterated 10^7 times to get the value of pi correct to 7 decimal places # pi=3.14159260358
+
+
+
+
+
 
