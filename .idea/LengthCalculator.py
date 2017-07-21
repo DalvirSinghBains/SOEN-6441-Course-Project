73946b43c2da79b444148afed33cdb906393eac0
DalvirSinghBains <dalvirsinghbains326@gmail.com>
2017-07-21 04:19:28 -0400

    Wrote the Text User Interface for CHEERS

--- .idea/LengthCalculator.py
+++ .idea/LengthCalculator.py
@@ -16,15 +16,27 @@ class LegthCalculator(object):
 
      #the value of alpha used in the equation is in radians
      #the cosine function included in the Python math library is used for calculating cos(x)
-     length= (2.0*radius*(1.0-math.cos(AlphaCalculator.alphaEstimateRadians/2.0)))
-     length1 = (2.0 * radius * (1.0 - math.cos(AlphaCalculator.alphaEstimateDegrees/ 2.0)))
+     length_for_radians= (2.0*radius*(1.0-math.cos(AlphaCalculator.alphaEstimateRadians/2.0)))
+     length_for_degrees = (2.0 * radius * (1.0 - math.cos(AlphaCalculator.alphaEstimateDegrees/ 2.0)))
 
-     print'The value of length is:'+str(length)+'(alpha in radians)'
-     print'The value of length is:'+ str(length1)+'(alpha in degrees)'
+     print'The value of length is:'+str(length_for_radians)+'(alpha in radians)'
+     print'The value of length is:'+ str(length_for_degrees)+'(alpha in degrees)'
 
 def main():
  lengthCalculatorObject=LegthCalculator()
- lengthCalculatorObject.calculateLength()
-
-
+ #lengthCalculatorObject.calculateLength()
+ user_input=1
+ while user_input==1:
+     print '-------Please select one option:-------'
+     print '1)Press 1 to calculate length!'
+     print '2)Press 2 to save output to XML file!'
+     print '3)Press 3 to view output XML file!'
+     print '4)Press 4 to exit the program!'
+     print('')
+     user_input_second = input()                     #Place input validations or exceptions here
+     if user_input_second == 1:
+         lengthCalculatorObject.calculateLength()
+     if user_input_second == 4:
+         user_input=0
+         exit(0)
 main()
